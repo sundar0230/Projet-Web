5d1e8eeb47a63b08b30456520a58c87395610c57
sundar <sundar91350@hotmail.fr>
2016-11-19 23:28:26 +0100

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161119130353) do
+ActiveRecord::Schema.define(version: 20161119193515) do
 
   create_table "beers", force: :cascade do |t|
     t.string   "name"
+    t.text     "description"
+    t.integer  "note"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "microposts", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "nom"
     t.text     "description"
     t.integer  "note"
     t.datetime "created_at",  null: false
